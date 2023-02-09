0491ab992e88dbae17b9a17deb8ccadb36938ad6
LinusWemmer <linus.wemmer@mailbox.tu-dresden.de>
2023-02-09 13:35:00 +0100

    added main function with input

--- getParse.py
+++ getParse.py
@@ -1,11 +1,16 @@
 import parzu_class as parzu
+import sys
 
-def get_parse():
+def get_parse(text: str):
     options = parzu.process_arguments()
     ParZu = parzu.Parser(options)
-    sentences = ParZu.main('Der Schüler gibt der Lehrerin sein Buch.')
-    for sentence in sentences:
+    sentences = ParZu.main(text)
+    #for sentence in sentences:
+    #    print(sentence)
+    return sentences
+
+# input should be a string
+if __name__ == "__main__":
+    input_text = sys.argv[1]
+    for sentence in get_parse(input_text):
         print(sentence)
-
-
-get_parse()
