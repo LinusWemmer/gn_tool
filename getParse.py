d6440ea1bd93dce42cca9e05876ad2b25192d586
LinusWemmer <linus.wemmer@mailbox.tu-dresden.de>
2023-02-10 11:17:36 +0100

    added functionality to mark nouns

--- getParse.py
+++ getParse.py
@@ -9,6 +9,18 @@ def get_parse(text: str):
     sentences = ParZu.main(text)
     return sentences
 
+def mark_nouns(sentences: list):
+    nouns = ""
+    for sentence in sentences:
+       words = sentence.split(" ")
+       for word in words:
+           word_parse = word.split("|")
+           if word_parse[1][:2] == "N.":
+               nouns += "<b>" + word_parse[0] + "</b> "
+           else:
+               nouns += word_parse[0] + " "
+    return nouns
+
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
@@ -16,10 +28,10 @@ def index():
     if request.method == "POST":
         input_text = request.form["inputText"]
         parse = get_parse(input_text)
-        return render_template("index.html", dataToRender=parse)
+        print(mark_nouns(parse))
+        return render_template("index.html", dataToRender=mark_nouns(parse))
     else:
         return render_template("index.html")
-    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
