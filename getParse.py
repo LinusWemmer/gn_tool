8df693b8d2a6719060e4973768b82cf324dc2194
LinusWemmer <linus.wemmer@mailbox.tu-dresden.de>
2023-02-13 15:42:47 +0100

    changed methods to work with conll and new model.

--- getParse.py
+++ getParse.py
@@ -7,19 +7,24 @@ def get_parse(text: str):
     options = parzu.process_arguments()
     ParZu = parzu.Parser(options)
     sentences = ParZu.main(text)
-    print(sentences)
     return sentences
 
 def mark_nouns(sentences: list):
     nouns = ""
     for sentence in sentences:
-       words = sentence.split(" ")
-       for word in words:
-           word_parse = word.split("|")
-           if word_parse[1][:2] == "N.":
-               nouns += "<b>" + word_parse[0] + "</b> "
-           else:
-               nouns += word_parse[0] + " "
+        words = sentence.split("\n")
+        words = words[:-2]
+        for word in words:
+            word_parse = word.split("\t")
+            print(word_parse)
+            print(word_parse[3])
+            if word_parse[3] == "N":
+                nouns += "<b>" + word_parse[1] + "</b> "
+            elif word_parse[3] == "$.":
+                nouns = nouns[:-1] + word_parse[1]
+            else:
+               nouns += word_parse[1] + " "
+            print(nouns)
     return nouns
 
 app = Flask(__name__)
@@ -39,9 +44,10 @@ def parse_text():
     if request.method == "POST":
         input_text = request.form["inputText"]
         parse = get_parse(input_text)
-        print(mark_nouns(parse))
-        return render_template("index.html", dataToRender=mark_nouns(parse))
-    return render_template("index.html")
+        marked_nouns = (mark_nouns(parse))
+        return render_template("index.html", dataToRender=marked_nouns)
+    else:
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
