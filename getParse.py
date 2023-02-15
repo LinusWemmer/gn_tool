2bc9f9c840d5a1736e498c02a0a72d29700766bd
LinusWemmer <linus.wemmer@mailbox.tu-dresden.de>
2023-02-15 12:17:14 +0100

    added mark form

--- getParse.py
+++ getParse.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
 import parzu_class as parzu
 import sys
@@ -17,8 +17,6 @@ def mark_nouns(sentences: list):
         words = words[:-2]
         for word in words:
             word_parse = word.split("\t")
-            print(word_parse)
-            print(word_parse[3])
             if word_parse[3] == "N":
                 input_form = f"""<input type="checkbox" id="noun{sentence_number}|{word_parse[0]}" name="noun{sentence_number}|{word_parse[0]}" value="select"> 
                 <label for="noun{sentence_number }|{word_parse[0]}">{"<mark>" + word_parse[1] + "</mark>"}</label> """
@@ -27,31 +25,34 @@ def mark_nouns(sentences: list):
                 nouns = nouns[:-1] + word_parse[1]
             else:
                nouns += word_parse[1] + " "
-            print(nouns)
         sentence_number += 1
     return nouns
+
+def get_dependencies(pos: int, words: list):
+    children = ""
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    if request.method == "POST":
-        input_text = request.form["inputText"]
-        parse = get_parse(input_text)
-        print(mark_nouns(parse))
-        return render_template("index.html", dataToRender=mark_nouns(parse))
-    else:
-        return render_template("index.html")
+    return render_template("index.html")
     
 @app.route("/parse", methods=["POST"])
 def parse_text():
     if request.method == "POST":
         input_text = request.form["inputText"]
         parse = get_parse(input_text)
+        print(parse)
         marked_nouns = (mark_nouns(parse))
         return render_template("index.html", dataToRender=marked_nouns)
     else:
         return render_template("index.html")
+    
+@app.route("/mark", methods=["POST"])
+def neutralize_marked():
+    selected_nouns = request.form
+    print(selected_nouns)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
