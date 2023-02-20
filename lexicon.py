83f359a39b2376d11e726b8dad3179283a3aa192
LinusWemmer <linus.wemmer@mailbox.tu-dresden.de>
2023-02-20 20:35:54 +0100

    added some small comment for fucntionality in the beginning

---
+++ lexicon.py
@@ -0,0 +1,22 @@
+class Lexicon:
+    # This class holds all the necessary information to construct the inclusivum
+    PRONOUNS = {"NOM": "en",
+                "POS": "ens",
+                "DAT": "em",
+                "AKK": "en"}
+    
+    ARTIKEL_DER = {"NOM": "de",
+                "POS": "ders",
+                "DAT": "derm",
+                "AKK": "de"}
+    ARTIKEL_EIN = {"NOM": "ein",
+                "POS": "einers",
+                "DAT": "einerm",
+                "AKK": "ein"}
+    ARTIKEL_JEDER = {"NOM": "jedey",
+                "POS": "jeders",
+                "DAT": "jederm",
+                "AKK": "jedey"}
+
+    def __init__(self):
+        pass
